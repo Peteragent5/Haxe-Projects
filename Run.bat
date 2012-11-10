0662cfcdcf79680dadb21b3715a852ad6307ec6f
Peter H <peterhalim@live.com>
2012-11-09 20:22:11 -0800

    Improved coding

---
+++ Run.bat
@@ -0,0 +1,9 @@
+@echo off
+for /f %%A in ('"prompt $H & echo on & for %%B in (1) do rem"') do set "bs=%%A"
+::set proj=
+set proj=Test
+::set /P proj=%bs% Project name: 
+if exist "%proj%\bin\%proj%.n" cd /D "%~dp0%proj%\bin" & neko %proj%.n
+if exist "%proj%\src\%proj%.n" cd /D "%~dp0%proj%\src" & neko %proj%.n
+
+::pause
